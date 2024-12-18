5cf666d67de9cef9b3b8132d4391d68e373f1266
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2024-12-17 23:16:14 -0500

    Math

--- 08_Math/RaizCuadrada.java
+++ 08_Math/RaizCuadrada.java
@@ -3,33 +3,24 @@ Escribe un programa que pida al usuario un número positivo y calcule su raíz
 cuadrada utilizando el método sqrt() de la clase Math. Si el número ingresado es
 negativo, muestra un mensaje adecuado en pantalla.
 */
-
 import java.util.Scanner;
 
 public class RaizCuadrada {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner superScanner = new Scanner(System.in);
 
-        // Solicitar al usuario que ingrese la base y el exponente
-        System.out.print("Ingresa la base: ");
-        double base = scanner.nextDouble();
+        System.out.println("Ingrese un numero positivo: ");
+        int numero = superScanner.nextInt();
 
-        System.out.print("Ingresa el exponente: ");
-        double exponente = scanner.nextDouble();
+        if (numero > 0) {
+            System.out.println("El resultado es: " + Math.sqrt(numero));
+        } else {
+            System.out.println(
+                "El numero " + numero + " ingresado no es valido"
+            );
+        }
 
-        // Calcular y mostrar el resultado usando el operador ternario
-        System.out.println(
-            (base == 0 && exponente == 0)
-                ? "La base y el exponente no pueden ser ambos cero."
-                : "El resultado de elevar " +
-                base +
-                " a la potencia " +
-                exponente +
-                " es: " +
-                Math.pow(base, exponente)
-        );
-
-        scanner.close();
+        superScanner.close();
     }
 }
